881c88fbadcce607c4c5d1a9fcff70cb7173e77c
saleebm <saleebmina@copt.dev>
2020-08-15 18:12:07 -0400

    feat: add google analytics

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     title: `NovelTea Cafe`,
     description: `Gather for the freshest kava and kratom tea, fresh brewed kombucha, and daily social events. What could be better? Answer: Nothing!`,
     author: `@minasa1eeb`,
+    siteUrl: 'www',
   },
   plugins: [
     {
@@ -93,7 +94,34 @@ module.exports = {
         },
       },
     },
-
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-175459740-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Defers execution of google analytics script after page load
+        defer: true,
+        // Any additional optional fields
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+      },
+    },
     {
       resolve: `gatsby-source-googlemaps-static`,
       options: {
@@ -113,6 +141,7 @@ module.exports = {
         ],
       },
     },
+    'gatsby-plugin-sitemap',
     `gatsby-plugin-offline`,
   ],
 }
