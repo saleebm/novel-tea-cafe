c8bf5bee0f26a304d253db23ef429203e6c7c0fb
saleebm <saleebmina@copt.dev>
2020-08-15 18:58:50 -0400

    fixes

--- site/src/components/structure/Layout/utils/wrap-page-element.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wrapPageElement = void 0;
-var react_1 = require("react");
-var Layout_1 = require("@Components/structure/Layout");
-exports.wrapPageElement = function (_a) {
-    var element = _a.element, props = _a.props;
-    return <Layout_1.Layout {...props}>{element}</Layout_1.Layout>;
-};
