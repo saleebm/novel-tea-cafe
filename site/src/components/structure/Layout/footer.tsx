bb19bc8b5da65d98d571c10ca32474288573efe5
saleebm <saleebmina@copt.dev>
2020-09-17 19:48:43 -0400

    updates

--- site/src/components/structure/Layout/footer.tsx
+++ site/src/components/structure/Layout/footer.tsx
@@ -6,9 +6,17 @@ import { SocialIcons } from '@Components/structure/Layout/social-icons'
 const useClasses = makeStyles((theme) => ({
   footerWrap: {
     position: 'relative',
-    height: 'min-content',
     minHeight: '100%',
     padding: `${theme.spacing(5)}px 0 ${theme.spacing(10)}px`,
+  },
+  socialIconWrap: {
+    width: '100%',
+    display: 'inline-flex',
+    flexFlow: 'row wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: theme.spacing(3),
+    filter: 'brightness(0.88) contrast(1.3)',
   },
 }))
 
@@ -26,7 +34,9 @@ export function Footer() {
           <Divider variant={'inset'} />
         </Grid>
         <Grid item xs={12}>
-          <SocialIcons />
+          <div className={classes.socialIconWrap}>
+            <SocialIcons />
+          </div>
         </Grid>
       </Grid>
     </Container>
